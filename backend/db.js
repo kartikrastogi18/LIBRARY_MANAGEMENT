4e79c5610a971113f8f2f9a49eb0ccef120bc938
kartikrastogi18 <kartikrastogi4544@gmail.com>
2025-10-26 11:39:44 +0530

    change-db

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,21 @@
-import {Sequelize} from "sequelize"
- const sequelize = new Sequelize("postgres","postgres","hD4k#6yQ",{
-    host:"localhost",
-    dialect:"postgres",
-    logging:false
-})
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize(
+  "library_management_quw4", // database name
+  "library_management_quw4_user", // username
+  "9jSSX1mtEUfGVeNAbWqX3eHBfeHZu9R0", // password
+  {
+    host: "dpg-d3urkhodl3ps73fbnqq0-a.oregon-postgres.render.com",
+    port: 5432,
+    dialect: "postgres",
+    logging: false,
+    dialectOptions: {
+      ssl: {
+        require: true,              // ✅ Render requires SSL
+        rejectUnauthorized: false,  // ✅ Bypass self-signed certificate issues
+      },
+    },
+  }
+);
+
 export default sequelize;
